522bc2ce17880cbf99668f8a0d74bcc376f789a7
Danny Yoo <dyoo@hashcollision.org>
2012-10-29 14:00:31 -0600

    coding more of the multidim library

--- multidim.rkt
+++ multidim.rkt
@@ -3,12 +3,29 @@
 (require (for-syntax syntax/parse 
                      racket/base 
                      racket/syntax)
-         "multidim-support.rkt")
+         "multidim-support.rkt"
+         racket/unsafe/ops)
 
-;; An implementation of a multidimensional array library
-(define-syntax (multidim stx)
+;; An implementation of a multidimensional array library, based on sequential access
+;; from TAOCP Chapter 1, Section 2.2.6.
+;;
+;; This provides a form called define-multidim that creates defintions for multidimensional
+;; array access.
+
+
+;; Compile-time helpers:
+(begin-for-syntax
+  (define (check-definitional-context! stx)
+    (define current-context (syntax-local-context))
+    (unless (or (eq? current-context 'top-level)
+                (eq? current-context 'module)
+                (internal-definition-context? current-context))
+      (raise-syntax-error #f "multidim can only be used in a definition context" stx))))
+
+
+(define-syntax (define-multidim stx)  
   
-  ;; fixme: ensure we are in a definitional context.
+  (check-definitional-context! stx)
   
   (syntax-parse stx
     [(_ name:id dims:expr ...)
@@ -17,50 +34,57 @@
                    [name-ref (format-id #'name "~a-ref" #'name)]
                    [name-set! (format-id #'name "~a-set!" #'name)]
                    [(dim-args ...) (generate-temporaries #'(dims ...))]
+                   [(ds ...) (generate-temporaries #'(dims ...))]
                    [(cs ...) (generate-temporaries #'(dims ...))])
        (with-syntax ([internal-name? (format-id #'internal-name "~a?" #'internal-name)]
                      [internal-name-data (format-id #'internal-name "~a-data" #'internal-name)])
          (syntax/loc stx
            (begin
+             
+             ;; We create an internal structure.  Users should not be able to construct
+             ;; it directly without going through the constructor
              (struct internal-name (data) #:transparent)
              (define-values (name name? name-ref name-set!)
-               (let*-values ([(ds) (list dims ...)]
-                             [(size) (apply * ds)]
-                             [(cs ...) (apply values (compute-coefficients ds))])
-                 (values 
-                  
-                  (lambda ()
-                    (internal-name (make-vector size)))
-                  
-                  (lambda (a-multi)
-                    (internal-name? a-multi))
-                  
-                  (lambda (a-multi dim-args ...)
-                    (unless (internal-name? a-multi)
-                      (raise-type-error 'name-set! (symbol->string 'name) a-multi))
-                    ;; FIXME: check ranges as well
-                    (vector-ref (internal-name-data a-multi)
-                                (+ (* dim-args cs) ...)))
-                  
-                  (lambda (a-multi dim-args ... v)
-                    (unless (internal-name? a-multi)
-                      (raise-type-error 'name-set! (symbol->string 'name) a-multi))
-                    ;; FIXME: check ranges as well
-                    (vector-set! (internal-name-data a-multi)
-                                 (+ (* dim-args cs) ...)
-                                 v)))))))))]))
-
-(multidim matrix 3 4)
-(define m (matrix))
-(matrix? m)
-(matrix-ref m 0 0)
-(matrix-set! m 0 0 42)
-(matrix-ref m 0 0)
-m
-
-(define ROWS 5)
-(define COLS 25)
-(multidim board ROWS COLS)
-(define b (board))
-(matrix? b)
-(board? b)
+               (let*-values ([(ds ...) (values dims ...)]
+                             [(size) (* ds ...)]
+                             [(cs ...) (apply values (compute-coefficients (list ds ...)))])
+                 
+                 ;; Early check: make sure the integers are all exact nonnegative integers:
+                 (unless (and (exact-nonnegative-integer? ds) ...)
+                   (raise-type-error 'name "exact nonnegative integers" (list ds ...)))
+                 
+                 ;; Also make sure size doesn't go beyond indexable size:
+                 (unless (fixnum? size)
+                   (raise-type-error 'name "dimension product of fixnum magnitude" size))
+                 
+                 ;; For the public functions, we want to check our types before
+                 ;; hitting the unsafe operations.
+                 (define (check-entry! who a-multi dim-args ...)
+                   (unless (internal-name? a-multi)
+                     (raise-type-error who (symbol->string 'name) a-multi))
+                   (begin (unless (and (exact-nonnegative-integer? dim-args)
+                                       (< dim-args ds))
+                            (raise-type-error who "indices within multidim bounds" (list dim-args ...)))
+                          ...))
+                 
+                 ;; Constructor
+                 (define (name)
+                   (internal-name (make-vector size)))
+                 
+                 ;; Getter
+                 (define (name-ref a-multi dim-args ...)
+                   (check-entry! 'name-ref a-multi dim-args ...)
+                   (unsafe-vector-ref (unsafe-struct-ref a-multi 0)
+                                      (unsafe-fx+ (unsafe-fx* dim-args cs) ...)))
+                 
+                 ;; Setter
+                 (define (name-set! a-multi dim-args ...)
+                   (check-entry! 'name-set! a-multi dim-args ...)
+                   (unsafe-vector-set! (unsafe-struct-ref a-multi 0)
+                                       (unsafe-fx+ (unsafe-fx* dim-args cs) ...)
+                                       v))
+                 
+                 (values name
+                         internal-name?
+                         name-ref
+                         name-set!)))))))]))
